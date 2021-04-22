725f4614c44cfbd2b4657ed3179c5abf7fede823
szguoyupeng <szguoyupeng@ininin.com>
2021-04-22 15:10:07 +0800

    删除venv

--- venv/Lib/site-packages/airtest/core/ios/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This package provide IOS Device Class.
-"""
-from airtest.core.ios.ios import IOS
--- venv/Lib/site-packages/pyswagger/errs.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-
-class CycleDetectionError(Exception): pass
-class ValidationError(Exception): pass
-class SchemaError(Exception): pass
-
